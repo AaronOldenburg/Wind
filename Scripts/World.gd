6a67f7f9f9d204ba038800df45ef05c2739913f9
Aaron Oldenburg <aoldenburg@ubalt.edu>
2020-06-14 21:01:05 -0400

    got spinning working!

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,8 +1,9 @@
-extends Area
+extends RigidBody
 
 var hands_in_ball: int = 0
 var hand_interacting : ARVRController = null
 var last_hand_pos : Vector3
+var turn_speed = 500
 
 var mat_main = preload("res://Graphics/Materials/Body.material")
 var mat_interact = preload("res://Graphics/Materials/world-interacting.material")
@@ -12,21 +13,27 @@ func _process(delta):
 		return
 	
 	var movement = hand_interacting.translation - last_hand_pos
-#	self.an
+	self.angular_velocity = Vector3(0,movement.x * turn_speed,movement.y)
 	
 	last_hand_pos = hand_interacting.translation
 
-func _on_World_body_entered(body):
+func _on_Area_body_entered(body):
 	hand_interacting = body.get_parent()
 	
 	hands_in_ball+=1
 	$MeshInstance.material_override = mat_interact
 
 
-func _on_World_body_exited(body):
+func _on_Area_body_exited(body):
 	if body.get_parent() == hand_interacting:
 		hand_interacting = null
 		
 	hands_in_ball-=1
-	if hands_in_ball == 0:
-		$MeshInstance.material_override = mat_main
+#	if hands_in_ball == 0:
+#		$MeshInstance.material_override = mat_main
+
+
+	pass # Replace with function body.
+
+
+	pass # Replace with function body.
