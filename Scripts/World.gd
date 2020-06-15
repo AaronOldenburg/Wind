cd0c49daff18b63cd10ac29bc7d9e6ebbbd02852
Aaron Oldenburg <aoldenburg@ubalt.edu>
2020-06-14 20:23:07 -0400

    still working to get globe spinning

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,12 +1,32 @@
 extends Area
 
+var hands_in_ball: int = 0
+var hand_interacting : ARVRController = null
+var last_hand_pos : Vector3
+
 var mat_main = preload("res://Graphics/Materials/Body.material")
 var mat_interact = preload("res://Graphics/Materials/world-interacting.material")
 
+func _process(delta):
+	if hand_interacting == null:
+		return
+	
+	var movement = hand_interacting.translation - last_hand_pos
+#	self.an
+	
+	last_hand_pos = hand_interacting.translation
+
 func _on_World_body_entered(body):
-	if body.is_class("CollisionShape"):
-		$MeshInstance.material_override = mat_interact
+	hand_interacting = body.get_parent()
+	
+	hands_in_ball+=1
+	$MeshInstance.material_override = mat_interact
 
 
 func _on_World_body_exited(body):
-	$MeshInstance.material_override = mat_main
+	if body.get_parent() == hand_interacting:
+		hand_interacting = null
+		
+	hands_in_ball-=1
+	if hands_in_ball == 0:
+		$MeshInstance.material_override = mat_main
